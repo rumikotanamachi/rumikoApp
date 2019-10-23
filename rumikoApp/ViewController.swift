8eab53f36cb86fee01bf34006b7588648fdf73b9
棚町瑠美子 <rumikotanamachi@gmail.com>
2019-10-23 13:37:56 +0900

    first commit

---
+++ rumikoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rumikoApp
+//
+//  Created by rumikotanamachi on 2019/10/23.
+//  Copyright © 2019 rumikotanamachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
